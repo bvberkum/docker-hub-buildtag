9376966434212128bac3b4a736c19048f89a32af
B. van Berkum <dev@dotmpe.com>
2017-10-29 01:42:02 +0200

    Scripts, added git-versioning [skip ci]

--- build.sh
+++ build.sh
@@ -9,6 +9,13 @@ test -e ca-certificates.crt || {
 
 test ! -e main || rm main
 
+# TODO: use golang-builder
+#docker run --rm \
+#  -v "$(pwd):/src" \
+#  -v /var/run/docker.sock:/var/run/docker.sock \
+#  centurylink/golang-builder \
+#  centurylink/hello:1.0
+
 go get github.com/PuerkitoBio/goquery &&
 go get github.com/hoisie/redis &&
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main . &&
@@ -17,4 +24,4 @@ docker build -t hubmon . &&
 echo "Starting test server" &&
 docker run --link redis:db -ti -p 8123:80 hubmon -redis db:6379 -cache-timeout 10
 
-# Id: x-docker-hub-build-monitor/0.0.1-dev
+# Id: x-docker-hub-build-monitor/0.0.2-dev
